e9ba6fbc813a287d5ba658a750ea63d02ed6ea23
Ryan-rsm-McKenzie <ryan__mckenzie@hotmail.com>
2019-02-18 01:12:41 -0800

    Update Util.cpp

--- Util.cpp
+++ Util.cpp
@@ -26,16 +26,16 @@ void SleepFor(std::size_t a_seconds)
 	pthread_cond_t conditionvar;
 	struct timespec timetoexpire;
 	if (pthread_mutex_init(&mutex, NULL)) {
-		return -1;
+		return;
 	}
 	if (pthread_cond_init(&conditionvar, NULL)) {
-		return -1;
+		return;
 	}
 	//When to expire is an absolute time, so get the current time and add
 	 //it to our delay time
 	timetoexpire.tv_sec = (unsigned int)time(NULL) + seconds;
 	timetoexpire.tv_nsec = 0;
-	return pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
+	pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
 #endif
 }
 
